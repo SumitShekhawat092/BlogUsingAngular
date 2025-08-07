69f28c6cb8672fb4dee49c128731d49f6345dc9b
SumitShekhawat092 <106969952+SumitShekhawat092@users.noreply.github.com>
2025-08-07 16:05:09 +0530

    Add BlogPost Page
    Add - Create Blogpost API - done
    Add - Angular Service and Consume API - done

--- UI/codepulse/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ UI/codepulse/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AddBlogSpot } from '../models/add-blog-post.model';
+import { BlogPostService } from '../Services/blog-post.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-blogpost',
@@ -8,7 +10,8 @@ import { AddBlogSpot } from '../models/add-blog-post.model';
 })
 export class AddBlogpostComponent {
   model: AddBlogSpot;
-  constructor(){
+  constructor(private blogPostService: BlogPostService,
+    private router: Router){
     this.model= {
       title: '',
       shortDescription:'',
@@ -22,6 +25,11 @@ export class AddBlogpostComponent {
   }
 
   onFormSubmit() : void{
-    console.log(this.model);
+     this.blogPostService.createBlogPost(this.model)
+     .subscribe({
+      next:(response) => {
+        this.router.navigateByUrl('/admin/blogposts');
+      }
+     });
   }
 }
